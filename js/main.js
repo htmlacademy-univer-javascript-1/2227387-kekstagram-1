9467014b1903b54bc3aa69d043f08ec4e7e9fd0b
sema <buglaev.sa@edu.spbstu.ru>
2022-11-03 13:23:31 +0300

    Start working with DOM

--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
-import {createPhotoDescription} from './data.js';
+import './drawing.js';
 
-/*
-созданиt массива из 25 сгенерированных объектов.
-Каждый объект массива — описание фотографии, описание генерируются с помощю функции createPhotoDescription()
-*/
-// eslint-disable-next-line no-unused-expressions, no-unused-vars
-const photos = Array.from({length: 25}, createPhotoDescription);
