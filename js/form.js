1dc45f9f085db28da75022f9bca9c1edf1d3c348
sema <buglaev.sa@edu.spbstu.ru>
2022-12-14 14:48:05 +0300

    1/2 done

--- js/form.js
+++ js/form.js
@@ -1,32 +1,63 @@
 import {isEscapeKey} from './utils.js';
 
+//form
 const uploadForm = document.querySelector('.img-upload__form');
-const editPictureForm = uploadForm.querySelector('.img-upload__overlay');
+//upload file
 const inputFile = uploadForm.querySelector('#upload-file');
-const description = uploadForm.querySelector('.text__description');
+
+//pictur redactor
+const editPictureForm = uploadForm.querySelector('.img-upload__overlay');
+//buttons
 const closeButton = editPictureForm.querySelector('#upload-cancel');
+const smallerScaleButton = editPictureForm.querySelector('.scale__control--smaller');
+const biggerScaleButton = editPictureForm.querySelector('.scale__control--bigger');
+
+//fields
+const scaleInput = editPictureForm.querySelector('.scale__control--value');
+const imgPreview = editPictureForm.querySelector('.img-upload__preview');
+const description = uploadForm.querySelector('.text__description');
 
 let pristine;
 
-loadForm();
-
-const onPopupEscKeydown = (evt) => {
+function onPopupEscKeydown(evt){
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closePictureRedactor();
   }
-};
+}
 
-const onPopupEscKeydownPrevent = (evt) => {
+function onPopupEscKeydownPrevent(evt){
   if (isEscapeKey(evt)) {
     evt.stopPropagation();
   }
-};
+}
 
-const onFormSubmit = (evt) => {
+function onFormSubmit(evt){
   evt.preventDefault();
   pristine.validate();
-};
+}
+
+function onSmallerButtonClick(){
+  const value = parseInt(scaleInput.value, 10) - 25;
+  if (value < 25) {
+    scaleInput.value = '25%';
+    imgPreview.style.transform = 'scale(0.25)';
+  } else {
+    scaleInput.value = `${value}%`;
+    imgPreview.style.transform = `scale(${value/100})`;
+  }
+}
+
+function onBiggerButtonClick(){
+  const value = parseInt(scaleInput.value, 10) + 25;
+  if (value > 100) {
+    scaleInput.value = '100%';
+    imgPreview.style.transform = 'scale(1)';
+  } else {
+    scaleInput.value = `${value}%`;
+    imgPreview.style.transform = `scale(${value/100})`;
+  }
+}
 
 function addListeners(){
   //добавим обработчики закрытия загружаемого изображения
@@ -34,10 +65,15 @@ function addListeners(){
   closeButton.addEventListener('click', closePictureRedactor, {once: true});
   //при нажатии Esc
   document.addEventListener( 'keydown', onPopupEscKeydown);
+
   //предотвращение закрытия если в фокусе поле для набора комментария
   description.addEventListener( 'keydown', onPopupEscKeydownPrevent);
 
+  //обработчики редактора изображения
+  smallerScaleButton.addEventListener('click', onSmallerButtonClick);
+  biggerScaleButton.addEventListener('click', onBiggerButtonClick);
 
+  //обработчик отправки формы
   uploadForm.addEventListener('submit', onFormSubmit);
 }
 
@@ -46,19 +82,28 @@ function removeListeners(){
 
   description.removeEventListener( 'keydown', onPopupEscKeydownPrevent);
 
+  smallerScaleButton.removeEventListener('click', onSmallerButtonClick);
+  biggerScaleButton.removeEventListener('click', onBiggerButtonClick);
+
   uploadForm.removeEventListener('submit', onFormSubmit);
+
+
 }
 
 
 function openPictureRedactor(){
   editPictureForm.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  //добавим валидатор
-  createpPristineValidator();
 
-  //добавим все нужные обработчики
+  //default values
+  scaleInput.value = '100%';
+  imgPreview.style.transform = 'scale(1)';
+
+  //add validator
+  createPristineValidator();
+
+  //add all listeners
   addListeners();
-
 }
 
 function closePictureRedactor(){
@@ -91,16 +136,18 @@ function validateHashtags(value){
   return uniqueArray.length === hashtagsArray.length;
 }
 
-function createpPristineValidator() {
+function createPristineValidator() {
   pristine = new Pristine(uploadForm, {
     classTo: 'img-upload__field-wrapper',
-    errorTextParent: 'img-upload__field-wrapper'
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextTag: 'span',
+    errorTextClass: 'form__error'
   }, false);
 
   pristine.addValidator(
     uploadForm.querySelector('.text__hashtags'),
     validateHashtags,
-    'Неправильно введены хэштеги'
+    'хэштеги должны быть уникальны, Хэштегов должно быть не больше 5, Хэштеги могут сордержать только буквы и цифры'
   );
 }
 
@@ -109,3 +156,4 @@ function loadForm(){
   inputFile.addEventListener('change', openPictureRedactor);
 }
 
+loadForm();
